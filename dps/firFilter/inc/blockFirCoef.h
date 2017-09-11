b3be214701ef2db38adce380cd6d5be9ecc330fa
ciric92 <ciricsvetislav@gmail.com>
2017-09-11 23:50:51 +0200

    ccs code and output comparison

---
+++ dps/firFilter/inc/blockFirCoef.h
@@ -0,0 +1,21 @@
+/*
+ * Experiment assembly implementation of block FIR filter - Chapter 3
+ * blockFirCoef.h
+ *
+ * Description: This is the filter coefficient file for assembly FIR filter
+ *
+ *  Created on: May 13, 2012
+ *      Author: BLEE
+ *
+ * 		For the book "Real Time Digital Signal Processing:
+ *                    Fundamentals, Implementation and Application, 3rd Ed"
+ * 		              By Sen M. Kuo, Bob H. Lee, and Wenshun Tian
+ * 		              Publisher: John Wiley and Sons, Ltd
+ */
+
+Int16 blockFirCoef[NUM_TAPS]={
+      -26,    -44,    322,   -476,   -219,   1852,  -2760,   -407,  18142,
+    18142,   -407,  -2760,   1852,   -219,   -476,    322,    -44,    -26
+};
+
+
